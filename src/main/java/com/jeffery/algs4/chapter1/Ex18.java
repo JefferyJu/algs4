4aa64c98beb099bb9182ea8e895274e5a2764698
jujf26056 <jujf26056@hundsun.com>
2018-11-12 17:57:12 +0800

    [feat]: add 18

---
+++ src/main/java/com/jeffery/algs4/chapter1/Ex18.java
@@ -0,0 +1,40 @@
+package com.jeffery.algs4.chapter1;
+
+/**
+ * 《算法》 第一章
+ * <p>
+ * 课后习题 1.1.18
+ *
+ * @author Jeffery_Ju
+ * @date 2018-11-12 17:20:40
+ */
+public class Ex18 {
+    public static void main(String[] args) {
+        // 50
+        System.out.println(mystery(2, 25));
+        // 33
+        System.out.println(mystery(3, 11));
+
+        System.out.println(mystery(10,20));
+
+    }
+
+    /**
+     * mystery(2, 25) 和 mystery(3, 11) 的返回值是多少？
+     * 给定正整数 a 和 b， mystery(a,b)计算的结果是什么？
+     * 将代码中的 + 替换为 * 并将 return 0 改为 return 1， 然后回答相同的问题。
+     *
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int mystery(int a, int b) {
+        if (b == 0) {
+            return 0;
+        }
+        if (b % 2 == 0) {
+            return mystery(a + a, b / 2);
+        }
+        return mystery(a + a, b / 2) + a;
+    }
+}
